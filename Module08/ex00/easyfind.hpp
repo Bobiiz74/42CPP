70f13099633b817459c1b3d95cf8fe7baa9f9983
Robin <robin.godtschalck@hotmail.fr>
2024-02-22 16:52:07 +0100

    m8 ex00 finito

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -6,13 +6,28 @@
 /*   By: robin <robin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 14:54:42 by robin             #+#    #+#             */
-/*   Updated: 2024/02/22 14:55:21 by robin            ###   ########.fr       */
+/*   Updated: 2024/02/22 16:37:44 by robin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+template <typename T>
+int easyfind(T& type, int toFound){
+    typename T::const_iterator it = std::find(type.begin(), type.end(), toFound);
+    if(it == type.end()){
+        std::cout << "No occurence founded for " << toFound << std::endl;
+        return(*it);
+    }
+    else
+        return (std::cout << "Occurence " << *it << " founded !" << std::endl, *it);
+}
 
 
 #endif
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -6,10 +6,36 @@
 /*   By: robin <robin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 14:54:02 by robin             #+#    #+#             */
-/*   Updated: 2024/02/22 14:54:12 by robin            ###   ########.fr       */
+/*   Updated: 2024/02/22 16:51:51 by robin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
 int main(){
-    
+    {
+        std::vector<int> vec(1);
+        vec.push_back(10);
+        vec.push_back(3);
+        vec.push_back(42);
+        easyfind(vec, 1);
+        easyfind(vec, 41);
+        easyfind(vec, 2);
+        easyfind(vec, 42);
+    }
+    std::cout << "---------------\n";
+    {
+        std::list<int> l(3);
+        l.push_back(2);
+        l.push_front(1);
+        l.push_back(41);
+        l.pop_back();
+        l.pop_front();
+        l.push_back(42);
+        easyfind(l, 1);
+        easyfind(l, 41);
+        easyfind(l, 2);
+        easyfind(l, 42);
+    }
+    return(0);
 }
