1673657005412ebeb78bab3564a923db447951c9
Robin <robin.godtschalck@hotmail.fr>
2024-03-21 16:32:05 +0100

    fix ex01 m9

--- Module09/ex01/main.cpp
+++ Module09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: robin <robin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 10:44:17 by robin             #+#    #+#             */
-/*   Updated: 2024/03/06 11:02:31 by robin            ###   ########.fr       */
+/*   Updated: 2024/03/21 16:19:46 by robin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
     if(argc == 2){
         RPN stack;
         std::string input = argv[1];
